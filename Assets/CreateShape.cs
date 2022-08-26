314696227c658ec2516980c34a7e0822777f0f08
Chaitanya Estarla <35031758+ChaitanyaEstarla@users.noreply.github.com>
2022-08-26 14:55:49 +0530

    final submit
    final submit before mailing

--- Assets/CreateShape.cs
+++ Assets/CreateShape.cs
@@ -15,24 +15,47 @@ public class CreateShape : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
     private Vector3 m_MousePosition;
     private LineRenderer m_LineRenderer;
     private int m_CurrentIndex = 0;
+    private Transform m_LastInstantiatedCollider;
 
     private void Start()
     {
         m_DrawnObject = new GameObject();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         if (!m_StartDrawing) return;
         var distance = m_MousePosition - Input.mousePosition;
         var distanceSquareMagnitude = distance.sqrMagnitude;
-        if (!(distanceSquareMagnitude > 500f)) return;
+        if (!(distanceSquareMagnitude > 2000)) return;
 
         //Draw Shape
         m_LineRenderer.SetPosition(m_CurrentIndex, mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z + 10)));
+        
+        if (m_LastInstantiatedCollider != null)
+        {
+            var currentPosition = m_LineRenderer.GetPosition(m_CurrentIndex);
+            m_LastInstantiatedCollider.LookAt(currentPosition);
+            m_LastInstantiatedCollider.gameObject.SetActive(true);
+
+            if (m_LastInstantiatedCollider.rotation.y == 0)
+            {
+                var eulerAngles = m_LastInstantiatedCollider.eulerAngles;
+                eulerAngles = new Vector3(eulerAngles.x, 90, eulerAngles.z);
+                m_LastInstantiatedCollider.eulerAngles = eulerAngles;
+            }
+            
+            var localScale = m_LastInstantiatedCollider.localScale;
+            localScale = new Vector3(localScale.x, Vector3.Distance(m_LastInstantiatedCollider.position, currentPosition) * 0.1f, localScale.z);
+            m_LastInstantiatedCollider.localScale = localScale;
+        }
+
+        m_LastInstantiatedCollider = Instantiate(cubePrefab, m_LineRenderer.GetPosition(m_CurrentIndex), Quaternion.identity, m_DrawnObject.transform);
+
         m_MousePosition = Input.mousePosition;
         m_CurrentIndex++;
         m_LineRenderer.positionCount = m_CurrentIndex + 1;
+        //position for line renderer
         m_LineRenderer.SetPosition(m_CurrentIndex, mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z + 10)));
     }
 
@@ -41,6 +64,7 @@ public class CreateShape : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
         m_StartDrawing = true;
         m_MousePosition = Input.mousePosition;
         
+        //Setup LineRenderer
         m_LineRenderer = m_DrawnObject.AddComponent<LineRenderer>();
         m_LineRenderer.startWidth = 0.2f;
         m_LineRenderer.material = lineMaterial;
@@ -51,6 +75,8 @@ public class CreateShape : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
         m_StartDrawing = false;
         m_DrawnObject.AddComponent<Rigidbody>();
         m_LineRenderer.useWorldSpace = false;
+        if(m_LastInstantiatedCollider != null)
+            Destroy(m_LastInstantiatedCollider.gameObject);
         Start();
         m_CurrentIndex = 0;
     }
